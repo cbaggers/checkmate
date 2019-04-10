62bc391835985f20eb6f070911faff2e2b0cecb8
cbaggers <chris.bagley@gmail.com>
2019-04-10 19:27:30 +0200

    simplify some print-type code

--- core/print.lisp
+++ core/print.lisp
@@ -2,8 +2,6 @@
 
 ;;------------------------------------------------------------
 
-(defgeneric print-type (type stream))
-
 (defmethod print-object ((obj ttype) stream)
   (format stream "#<NAKED-TYPE ~a>" (type-of obj)))
 
@@ -11,20 +9,19 @@
   (format stream "#<NAKED-PARAMETER ~a>" (slot-value obj 'name)))
 
 (defmethod print-object ((obj type-ref) stream)
-  (print-type (deref obj) stream))
+  (force-output t)
+  (let ((desig (ttype-of obj)))
+    (format stream "#T~a" desig)))
 
-(defmethod print-type ((obj ttype) stream)
-  ;; something super fucky here when printing circular types
-  ;; regarless of what settings I use I get the stackoverflow
-  ;; it's not the stream .. it's something dynamic
-  (format stream "#T~a" (designator-from-type obj)))
-
-(defmethod print-type ((obj ttype-parameter) stream)
-  (if (eq (slot-value obj 'name) 'ttype)
-      (print-type (deref (slot-value obj 'value)))
-      (format stream "#P~a"
-              (if (typep obj 'unknown-param)
-                  (slot-value obj 'name)
-                  (slot-value obj 'value)))))
+(defgeneric print-type (type stream)
+  (:method ((obj ttype) stream)
+    (format stream "#T~a" (designator-from-type obj)))
+  (:method ((obj ttype-parameter) stream)
+    (if (eq (slot-value obj 'name) 'ttype)
+        (print-type (deref (slot-value obj 'value)))
+        (format stream "#P~a"
+                (if (typep obj 'unknown-param)
+                    (slot-value obj 'name)
+                    (slot-value obj 'value))))))
 
 ;;------------------------------------------------------------
